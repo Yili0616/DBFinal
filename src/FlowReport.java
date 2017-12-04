0b99ff140e86525b01aae8cc23074c5131f53270
Yili0616 <yilihui@gatech.edu>
2017-12-03 23:02:10 -0500

    TRY AGAIN

--- src/FlowReport.java
+++ src/FlowReport.java
@@ -111,76 +111,106 @@ public class FlowReport {
 //        }
 //        return true;
         ConnectionConfig con = new ConnectionConfig();
-        System.out.println("SELECT Station.StopID, Station.Name, COUNT( Station.StopID ) As CountIn, SUM(Trip.Tripfare) As Rev\n" +
-                "FROM Trip\n" +
-                "INNER JOIN Station ON Trip.StartsAt = Station.StopID\n" +
-                "WHERE StartTime >= '" + startTime + "'\n" +
-                "AND StartTime <= '" + endTime + "'\n" +
-                "GROUP BY StartsAt");
-        // Get all the "start" station that sends out passengers in the time interval
-        ResultSet start = con.getResult("SELECT Station.StopID, Station.Name, COUNT( Station.StopID ) As CountIn, SUM(Trip.Tripfare) As Rev\n" +
-                "FROM Trip\n" +
-                "INNER JOIN Station ON Trip.StartsAt = Station.StopID\n" +
-                "WHERE StartTime >= '" + startTime + "'\n" +
-                "AND StartTime <= '" + endTime + "'\n" +
-                "GROUP BY StartsAt");
-//        System.out.println("SELECT Station.StopID, Station.Name, COUNT( Station.StopID ) As CountIn, SUM(Station.EnterFare) As Rev\n" +
+        String sql1 = "(SELECT Name, PassengerIn, PassengerOut, Revenue "
+        + "FROM (SELECT StopID,Name, COUNT(*) AS PassengerIn,Sum(TripFare) AS Revenue "
+        + "FROM Trip JOIN Station ON Trip.StartsAt=Station.StopID "
+        + "WHERE StartTime>='"+startTime+ "' AND StartTime<='" +endTime +"' "
+        +" GROUP BY StartsAt) AS S "
+        + "NATURAL LEFT OUTER JOIN "
+        + "(SELECT StopID,Name, COUNT(*) AS PassengerOut "
+        + "FROM Trip JOIN Station ON Trip.EndsAt=Station.StopID "
+        +" WHERE StartTime>=' "+startTime+ "' AND StartTime<='" + endTime + "' "
+        + "GROUP BY EndsAt) AS F "
+        + "WHERE Name IS NOT NULL) "
++ ")UNION(" +
+          "SELECT Name, PassengerIn, PassengerOut, Revenue "
+                        + "FROM (SELECT StopID,Name, COUNT(*) AS PassengerIn,Sum(TripFare) AS Revenue "
+                        + "FROM Trip JOIN Station ON Trip.StartsAt=Station.StopID "
+                        + "WHERE StartTime>='"+ startTime+ "' AND StartTime<='"+ endTime+ "'"
+                       + " GROUP BY StartsAt) AS S "
+        + "NATURAL RIGHT OUTER JOIN "
+        + "(SELECT StopID,Name, COUNT(*) AS PassengerOut "
+        + "FROM Trip JOIN Station ON Trip.EndsAt=Station.StopID "
+        + "WHERE StartTime>= '"+startTime + "'AND StartTime<='"+ endTime+ "' "
+         + "GROUP BY EndsAt) AS F "
+        + "WHERE Name IS NOT NULL)";
+
+        ResultSet rs1 = con.getResult(sql1);
+
+
+
+        //        System.out.println("SELECT Station.StopID, Station.Name, COUNT( Station.StopID ) As CountIn, SUM(Trip.Tripfare) As Rev\n" +
 //                "FROM Trip\n" +
 //                "INNER JOIN Station ON Trip.StartsAt = Station.StopID\n" +
 //                "WHERE StartTime >= '" + startTime + "'\n" +
 //                "AND StartTime <= '" + endTime + "'\n" +
 //                "GROUP BY StartsAt");
+//        // Get all the "start" station that sends out passengers in the time interval
+//        ResultSet start = con.getResult("SELECT Station.StopID, Station.Name, COUNT( Station.StopID ) As CountIn, SUM(Trip.Tripfare) As Rev\n" +
+//                "FROM Trip\n" +
+//                "INNER JOIN Station ON Trip.StartsAt = Station.StopID\n" +
+//                "WHERE StartTime >= '" + startTime + "'\n" +
+//                "AND StartTime <= '" + endTime + "'\n" +
+//                "GROUP BY StartsAt");
+////        System.out.println("SELECT Station.StopID, Station.Name, COUNT( Station.StopID ) As CountIn, SUM(Station.EnterFare) As Rev\n" +
+////                "FROM Trip\n" +
+////                "INNER JOIN Station ON Trip.StartsAt = Station.StopID\n" +
+////                "WHERE StartTime >= '" + startTime + "'\n" +
+////                "AND StartTime <= '" + endTime + "'\n" +
+////                "GROUP BY StartsAt");
+//
+//        // Get all the corresponding "end" station in the time interval
+////        ResultSet end = con1.getResult("SELECT Station.StopID, Station.Name, COUNT(Station.StopID) As CountOut" +
+////                "FROM Trip INNER JOIN Station ON Trip.EndsAt = Station.StopID " +
+////                "WHERE StartTime >= '"+ startTime + "'AND StartTime <= '"
+////                + endTime + "'GROUP BY StartsAt");
+//
+//        ResultSet end = con.getResult("SELECT Station.StopID, Station.Name, COUNT(Station.StopID) As CountOut FROM Trip INNER JOIN Station ON Trip.EndsAt = Station.StopID WHERE StartTime >= '"+ startTime + "'AND StartTime <= '" + endTime + "'GROUP BY StartsAt");
 
-        // Get all the corresponding "end" station in the time interval
-//        ResultSet end = con1.getResult("SELECT Station.StopID, Station.Name, COUNT(Station.StopID) As CountOut" +
-//                "FROM Trip INNER JOIN Station ON Trip.EndsAt = Station.StopID " +
-//                "WHERE StartTime >= '"+ startTime + "'AND StartTime <= '"
-//                + endTime + "'GROUP BY StartsAt");
 
-        ResultSet end = con.getResult("SELECT Station.StopID, Station.Name, COUNT(Station.StopID) As CountOut FROM Trip INNER JOIN Station ON Trip.EndsAt = Station.StopID WHERE StartTime >= '"+ startTime + "'AND StartTime <= '" + endTime + "'GROUP BY StartsAt");
-
-        System.out.println("SELECT Station.StopID, Station.Name, COUNT(Station.StopID) As CountOut FROM Trip INNER JOIN Station ON Trip.EndsAt = Station.StopID WHERE StartTime >= '"+ startTime + "'AND StartTime <= '" + endTime + "'GROUP BY StartsAt");
-
+//        System.out.println("SELECT Station.StopID, Station.Name, COUNT(Station.StopID) As CountOut FROM Trip INNER JOIN Station ON Trip.EndsAt = Station.StopID WHERE StartTime >= '"+ startTime + "'AND StartTime <= '" + endTime + "'GROUP BY StartsAt");
+        ResultSet rs = con.getResult(sql1);
         data = FXCollections.observableArrayList();
         try {
-            System.out.println("try!");
-            if (!start.next()) {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Error");
-                alert.setHeaderText("Error Operation!");
-                alert.setContentText("There is no trip");
-                alert.showAndWait();
-            }
+
+//            System.out.println("try!");
+//            if (!start.next()) {
+//                Alert alert = new Alert(Alert.AlertType.ERROR);
+//                alert.setTitle("Error");
+//                alert.setHeaderText("Error Operation!");
+//                alert.setContentText("There is no trip");
+//                alert.showAndWait();
+//            }
 //            System.out.println(start.next());
 //            System.out.println(end.next());
-            else {
-                start.beforeFirst();
-                while (start.next()) {
-                    end.next();
-                    // Calculate passenger Flow
-                    System.out.println("create table!");
-                    int a = start.getInt("CountIn");
-                    System.out.println(a);
-                    int b = end.getInt("CountOut");
-                    System.out.println(b);
-                    int CalcFlow = a - b;
-//                int CalcFlow = start.getInt("countIn") - end.getInt("countOut");
-                    System.out.println("flow:" + CalcFlow);
+//            else {
+//                start.beforeFirst();
+//                while (start.next()) {
+//                    end.next();
+//                    // Calculate passenger Flow
+//                    System.out.println("create table!");
+//                    int a = start.getInt("CountIn");
+//                    System.out.println(a);
+//                    int b = end.getInt("CountOut");
+//                    System.out.println(b);
+//                    int CalcFlow = a - b;
+////                int CalcFlow = start.getInt("countIn") - end.getInt("countOut");
+//                    System.out.println("flow:" + CalcFlow);
+//
+//                    data.add(new ReportInfo(start.getString("Name"), start.getInt("CountIn"),  end.getInt("CountOut"), CalcFlow, start.getDouble("Rev")));
+//
+//                    columnStationName.setCellValueFactory(new PropertyValueFactory<>("StationName"));
+//                    columnPassengerIn.setCellValueFactory(new PropertyValueFactory<>("PassengerIn"));
+//                    columnPassengerOut.setCellValueFactory(new PropertyValueFactory<>("PassengerOut"));
+//                    columnFlow.setCellValueFactory(new PropertyValueFactory<>("Flow"));
+//                    columnRevenue.setCellValueFactory(new PropertyValueFactory<>("Revenue"));
+//
+//                    report.setItems(null);
+//                    report.setItems(data);
+//                }
+//            }
 
-                    data.add(new ReportInfo(start.getString("Name"), start.getInt("CountIn"),  end.getInt("CountOut"), CalcFlow, start.getDouble("Rev")));
-
-                    columnStationName.setCellValueFactory(new PropertyValueFactory<>("StationName"));
-                    columnPassengerIn.setCellValueFactory(new PropertyValueFactory<>("PassengerIn"));
-                    columnPassengerOut.setCellValueFactory(new PropertyValueFactory<>("PassengerOut"));
-                    columnFlow.setCellValueFactory(new PropertyValueFactory<>("Flow"));
-                    columnRevenue.setCellValueFactory(new PropertyValueFactory<>("Revenue"));
-
-                    report.setItems(null);
-                    report.setItems(data);
-                }
-            }
-
-        } catch (SQLException e) {
+        } catch (Exception e) {
             System.err.println(e.getMessage());
 //            System.out.println("wrong!!!!!!!!!!!!!!!!!!!");
 //            Alert alert = new Alert(Alert.AlertType.ERROR);
